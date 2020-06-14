ea896e2200a807266f4b00538c7ee41db3f3b58e
admig <153513165@qq.com>
2020-06-14 16:35:01 +0800

    修改

--- mq_queue_pro/src/main/java/com/project/MqQueueProApplication.java
+++ mq_queue_pro/src/main/java/com/project/MqQueueProApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class MqQueueProApplication {
 
     public static void main(String[] args) {
+        System.out.println("asdfd");
         SpringApplication.run(MqQueueProApplication.class, args);
     }
 
